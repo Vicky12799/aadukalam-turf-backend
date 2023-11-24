4ceb306aab53c05ea638eb4878ddec32217e24bb
Vicky12799 <vicky12799@gmail.com>
2023-11-24 14:36:49 +0530

    Reorganized folder structure

--- apps/manager/models.py
+++ apps/manager/models.py
@@ -1,15 +1,15 @@
 # Create your models here.
 from django.db import models
 
+
 class Manager(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
-    secondary_phone_number = models.CharField(max_length=15, blank=True, null=True)
+    secondary_phone_number = models.CharField(max_length=15, blank=True)
     mail_id = models.EmailField()
-
 
     def __str__(self):
         return self.name
